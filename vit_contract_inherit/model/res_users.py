ca065589b0682f8f4e4e2f2cd7af80b94c0e762f
Muhammad Syahril Darmawan <syahrildrmwn@gmail.com>
2025-10-16 14:50:58 +0700

    [ADD] Fungsi untuk Allowed Kanwil dan Allowed Kanca

--- vit_contract_inherit/model/res_users.py
+++ vit_contract_inherit/model/res_users.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError, UserError
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
@@ -6,12 +7,47 @@ class ResUsers(models.Model):
     kanca_id = fields.Many2one(
         'vit.kanca',
         string='Kanca',
+        required=True,
     )
-
 
     kanwil_id = fields.Many2one(
         'vit.kanwil', 
-        string='Kanwil'
+        string='Kanwil',
+        required=True,
+    )
+
+    allowed_kanwil_id = fields.Many2one(
+        'vit.kanwil', 
+        string='Allowed Kanwil',
+        required=True,
+    )
+
+    allowed_kanca_id = fields.Many2one(
+        'vit.kanca',
+        string='Allowed Kanca',
+    )
+
+    allowed_kanca_domain_ids = fields.Many2many(
+        'vit.kanca',
+        compute='_compute_allowed_kanca_domain',
+        string='Allowed Kanca Domain',
+    )
+
+
+
+
+
+
+
+
+    is_allowed_kanca_group = fields.Boolean(
+        string='Is Allowed Kanca Group',
+        compute='_compute_is_allowed_kanca_group'
+    )
+
+    is_allowed_kanwil_group = fields.Boolean(
+        string='Is Allowed Kanwil Group',
+        compute='_compute_is_allowed_kanwil_group'
     )
 
     is_kanca_group = fields.Boolean(
@@ -24,6 +60,10 @@ class ResUsers(models.Model):
         string='Is Kanwil Group', 
         compute='_compute_is_kanwil_group'
     )
+
+
+
+
 
     @api.depends('groups_id')
     def _compute_is_kanca_group(self):
@@ -38,3 +78,99 @@ class ResUsers(models.Model):
             rec.is_kanwil_group = rec.has_group('vit_contract_inherit.group_vit_contract_kanwil')
             if not rec.is_kanwil_group:
                 rec.kanwil_id = False
+
+
+
+    # @api.depends('groups_id')
+    # def _compute_is_allowed_kanca_group(self):
+    #     for rec in self:
+    #         rec.is_allowed_kanca_group = rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanca')
+    #         if not rec.is_allowed_kanca_group:
+    #             rec.allowed_kanca_id = False
+
+
+
+    # @api.depends('groups_id')
+    # def _compute_is_allowed_kanwil_group(self):
+    #     for rec in self:
+    #         rec.is_allowed_kanwil_group = rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanwil')
+    #         if not rec.is_allowed_kanwil_group:
+    #             rec.allowed_kanwil_id = False
+
+
+
+    @api.depends('groups_id')
+    def _compute_is_allowed_kanca_group(self):
+        for rec in self:
+            has_allowed_kanca = rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanca')
+            has_user = rec.has_group('vit_contract.group_vit_contract_user')
+            rec.is_allowed_kanca_group = has_allowed_kanca and has_user
+            if not rec.is_allowed_kanca_group:
+                rec.allowed_kanca_id = False
+
+    @api.depends('groups_id')
+    def _compute_is_allowed_kanwil_group(self):
+        for rec in self:
+            has_allowed_kanwil = rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanwil')
+            has_user = rec.has_group('vit_contract.group_vit_contract_user')
+            rec.is_allowed_kanwil_group = has_allowed_kanwil and has_user
+            if not rec.is_allowed_kanwil_group:
+                rec.allowed_kanwil_id = False
+
+
+
+    @api.onchange('allowed_kanwil_id')
+    def _onchange_allowed_kanwil_id(self):
+        """Reset allowed_kanca_id hanya jika allowed_kanwil_id benar-benar berubah ke yang lain."""
+        for rec in self:
+            if not rec._origin or not rec._origin.allowed_kanwil_id:
+                continue
+            old_kanwil = rec._origin.allowed_kanwil_id
+            new_kanwil = rec.allowed_kanwil_id
+            if old_kanwil and new_kanwil and old_kanwil.id != new_kanwil.id:
+                rec.allowed_kanca_id = False
+            elif not new_kanwil:
+                rec.allowed_kanca_id = False
+                
+
+
+    @api.depends('allowed_kanwil_id', 'groups_id')
+    def _compute_allowed_kanca_domain(self):
+        for rec in self:
+            domain = []
+            if rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanwil'):
+                if rec.allowed_kanwil_id:
+                    domain = [('kanwil_id', '=', rec.allowed_kanwil_id.id)]
+                else:
+                    domain = [('id', '=', 0)] 
+            elif rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanca'):
+                domain = [] 
+            else:
+                domain = [('id', '=', 0)] 
+
+            rec.allowed_kanca_domain_ids = self.env['vit.kanca'].search(domain)
+
+
+
+
+
+
+    @api.constrains('groups_id')
+    def _check_allowed_dependencies(self):
+        for rec in self:
+            errors = []
+
+            has_user = rec.has_group('vit_contract.group_vit_contract_user')
+            has_allowed_kanca = rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanca')
+            has_allowed_kanwil = rec.has_group('vit_contract_inherit.group_vit_contract_allowed_kanwil')
+
+            if (has_allowed_kanca or has_allowed_kanwil) and not has_user:
+                if has_allowed_kanca and has_allowed_kanwil:
+                    errors.append("Group 'Allowed Kanwil' dan 'Allowed Kanca' hanya bisa diaktifkan jika Group 'User' juga aktif.")
+                elif has_allowed_kanca:
+                    errors.append("Group 'Allowed Kanca' hanya bisa diaktifkan jika Group 'User' juga aktif.")
+                elif has_allowed_kanwil:
+                    errors.append("Group 'Allowed Kanwil' hanya bisa diaktifkan jika Group 'User' juga aktif.")
+
+            if errors:
+                raise ValidationError("\n".join(errors))
