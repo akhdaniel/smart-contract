5eaeec4af421c540654f394950524551e2dff9a9
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-10-19 04:55:38 +0700

    Update App.vue

--- vit-portal-vue/src/App.vue
+++ vit-portal-vue/src/App.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <a class="navbar-brand" href="/contracts"><i class="fa-solid fa-house me-2"></i>SmartContract Vendor Portal</a>
         <div v-if="authStore.isLoggedIn" class="d-flex align-items-center">
-          <span class="me-3 fg-white">Welcome, {{ authStore.user.name }}</span>
+          <span class="me-3 text-secondary">Welcome, {{ authStore.user.name }}</span>
           <button @click="logout" class="btn btn-outline-secondary">Logout</button>
         </div>
       </div>
