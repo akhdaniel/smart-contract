ff009e0be2158ce6163ed49acaaa482397f6452d
Syahril Darmawan <syahrildrmwn@gmail.com>
2026-03-05 09:12:19 +0700

    Revert "testing portal vue"
    This reverts commit fc6e75981bf03a6d2084dd942a155ac4e5025bf1.

--- vit-portal-vue/vite.config.js
+++ vit-portal-vue/vite.config.js
@@ -10,13 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    proxy: {
-      '/web': {
-        target: 'http://localhost:8217',
-        changeOrigin: true
-      }
-    }
   }
 })
