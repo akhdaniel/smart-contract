201ce13ce00a84baf4f2e934107562cb9f2f8dd6
Syahril Darmawan <syahrildrmwn@gmail.com>
2026-03-10 08:51:51 +0700

    Reapply "[Testing infromasi pembayaran]"
    This reverts commit b71bbad0b8b0a40599423babf9d5e650a6e88d15.

--- vit-portal-vue/vite.config.js
+++ vit-portal-vue/vite.config.js
@@ -1,21 +1,14 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  server: {
-    proxy: {
-      '/web': {
-        target: 'http://localhost:8217',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  }
 })
