be0885c41b45b0cbd18c8524d2058032e5cf5ecb
Syahril Darmawan <syahrildrmwn@gmail.com>
2026-03-11 08:59:01 +0700

    tes

--- vit-portal-vue/vite.config.js
+++ vit-portal-vue/vite.config.js
@@ -10,5 +10,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index-e324a293.js'
+      }
+    }
   }
 })
